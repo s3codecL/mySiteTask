4e124952b658a58ffd245d71cfa7eaae8b7e9d1c
s3code <gandradev@gmail.com>
2024-08-22 02:26:42 -0400

    template pass data - about.html, index.html, projects.html and def all queryset

--- myapp/views.py
+++ myapp/views.py
@@ -4,16 +4,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    title = 'Welcome to Django!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 def hello(request, username):
     print(type(username))
     return HttpResponse('<h1>Hello %s</h1>' % username)
 def about(request):
-    return render(request, 'about.html')
+    username = 's3codecL'
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return render(request, 'projects.html')
+    #projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
 def tasks(request):
     #task = get_object_or_404(Task, title=title)
