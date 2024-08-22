4081b8722cba771b6f1fbfcfa4e5f4a85c55d223
s3code <gandradev@gmail.com>
2024-08-22 01:36:41 -0400

    add params & models

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('about/', views.about),
     path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/<str:title>/', views.tasks, name='tasks'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -8,3 +10,11 @@ def hello(request, username):
     return HttpResponse('<h1>Hello %s</h1>' % username)
 def about(request):
     return HttpResponse('About')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    task = get_object_or_404(Task, title=title)
+    return HttpResponse('tasks: %s' % task.title)
