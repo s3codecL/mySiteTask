5ff549aee080a02ca7a27d80d7a313409bb7c232
s3code <gandradev@gmail.com>
2024-08-22 02:15:51 -0400

    render - create templates - index.html, about.html, projects.html, task.html - upd urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about/', views.about),
     path('hello/<str:username>', views.hello),
     path('projects/', views.projects),
-    path('tasks/<str:title>/', views.tasks, name='tasks'),
+    path('tasks/', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,20 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Page")
+    return render(request, "index.html")
 def hello(request, username):
     print(type(username))
     return HttpResponse('<h1>Hello %s</h1>' % username)
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'about.html')
 
 def projects(request):
     projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html')
 
-def tasks(request, title):
-    task = get_object_or_404(Task, title=title)
-    return HttpResponse('tasks: %s' % task.title)
+def tasks(request):
+    #task = get_object_or_404(Task, title=title)
+    return render(request, 'task.html')
