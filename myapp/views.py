9cb14edc4d4bd51dc5ff652f4fea4cb88d89cc9b
s3code <gandradev@gmail.com>
2024-08-22 05:05:01 -0400

    forms add CSRF token to the POST method in create_task.html - Implemented a new view to render the request object. Created a new class for create_task.html and new migration

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateNewTask
 
 # Create your views here.
 def index(request):
@@ -30,3 +31,14 @@ def tasks(request):
     return render(request, 'task.html', {
         'tasks': tasks
     })
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(title=request.POST['title'],
+                            description=request.POST['description'], project_id=2)
+        return redirect('/tasks/')
+
