44fff39e00cbba037302685b81aa8c9eb9ef2462
s3code <gandradev@gmail.com>
2024-08-23 05:26:59 -0400

    Add Forms POST create_project.html. Add new class CreateNewProject. Reorganized directory structure in templates. Upd navigation in base.html. Add create_project.html template. Modified view accordingly.

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from .models import Project, Task
 from django.shortcuts import render, redirect
-from .forms import CreateNewTask
+from .forms import CreateNewTask, CreateNewProject
 
 # Create your views here.
 def index(request):
@@ -21,24 +21,37 @@ def about(request):
 def projects(request):
     #projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html', {
+    return render(request, 'project/projects.html', {
         'projects': projects
     })
 
 def tasks(request):
     #task = get_object_or_404(Task, title=title)
     tasks = Task.objects.all()
-    return render(request, 'task.html', {
+    return render(request, 'task/task.html', {
         'tasks': tasks
     })
 
 def create_task(request):
     if request.method == 'GET':
-        return render(request, 'create_task.html', {
+        return render(request, 'task/create_task.html', {
             'form': CreateNewTask()
         })
     else:
         Task.objects.create(title=request.POST['title'],
                             description=request.POST['description'], project_id=2)
         return redirect('/tasks/')
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'project/create_project.html', {
+            'form': CreateNewProject()
+        })
+    else:
+        project = Project.objects.create(name=request.POST['name'])
+        print(project)
+        return render(request, 'project/create_project.html', {
+            'form': CreateNewProject()
+        })
 
